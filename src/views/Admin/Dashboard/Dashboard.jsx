d69bd4ed5c3510661819ef4bfd8a6342db656c2b
kodri01 <kodrimukhtar5@gmail.com>
2025-02-05 11:59:43 +0700

    fix: update permission in dashboard

--- src/views/Admin/Dashboard/Dashboard.jsx
+++ src/views/Admin/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import LayoutAdmin from "../../../layouts/Admin";
 import Api from "../../../services/Api";
 import Cookies from "js-cookie";
 import { Link } from "react-router-dom";
+import hasAnyPermission from "../../../utils/Permissions";
 
 export default function Dashboard() {
   //title Page
@@ -35,81 +36,89 @@ export default function Dashboard() {
       <main>
         <div className="container-fluid px-4 mt-5">
           <div className="row">
-            <div className="col-xl-3 col-md-6">
-              <div className="card bg-primary text-white mb-4 border-0 shadow-0">
-                <div className="card-body">
-                  <strong>{countCategory}</strong> CATEGORIES
-                </div>
-                <div className="card-footer d-flex align-items-center justify-content-between">
-                  <Link
-                    className="small text-white stretched-link"
-                    to="/admin/categories"
-                  >
-                    View Details
-                  </Link>
-                  <div className="small text-white">
-                    <i className="fas fa-angle-right"></i>
+            {hasAnyPermission(["categories.index"]) && (
+              <div className="col-xl-3 col-md-6">
+                <div className="card bg-primary text-white mb-4 border-0 shadow-0">
+                  <div className="card-body">
+                    <strong>{countCategory}</strong> CATEGORIES
+                  </div>
+                  <div className="card-footer d-flex align-items-center justify-content-between">
+                    <Link
+                      className="small text-white stretched-link"
+                      to="/admin/categories"
+                    >
+                      View Details
+                    </Link>
+                    <div className="small text-white">
+                      <i className="fas fa-angle-right"></i>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            )}
 
-            <div className="col-xl-3 col-md-6">
-              <div className="card bg-warning text-white mb-4 border-0 shadow-0">
-                <div className="card-body">
-                  <strong>{countPosts}</strong> POSTS
-                </div>
-                <div className="card-footer d-flex align-items-center justify-content-between">
-                  <Link
-                    className="small text-white stretched-link"
-                    to="/admin/posts"
-                  >
-                    View Details
-                  </Link>
-                  <div className="small text-white">
-                    <i className="fas fa-angle-right"></i>
+            {hasAnyPermission(["posts.index"]) && (
+              <div className="col-xl-3 col-md-6">
+                <div className="card bg-warning text-white mb-4 border-0 shadow-0">
+                  <div className="card-body">
+                    <strong>{countPosts}</strong> POSTS
+                  </div>
+                  <div className="card-footer d-flex align-items-center justify-content-between">
+                    <Link
+                      className="small text-white stretched-link"
+                      to="/admin/posts"
+                    >
+                      View Details
+                    </Link>
+                    <div className="small text-white">
+                      <i className="fas fa-angle-right"></i>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            )}
 
-            <div className="col-xl-3 col-md-6">
-              <div className="card bg-success text-white mb-4 border-0 shadow-0">
-                <div className="card-body">
-                  <strong>{countProducts}</strong> PRODUCTS
-                </div>
-                <div className="card-footer d-flex align-items-center justify-content-between">
-                  <Link
-                    className="small text-white stretched-link"
-                    to="/admin/products"
-                  >
-                    View Details
-                  </Link>
-                  <div className="small text-white">
-                    <i className="fas fa-angle-right"></i>
+            {hasAnyPermission(["products.index"]) && (
+              <div className="col-xl-3 col-md-6">
+                <div className="card bg-success text-white mb-4 border-0 shadow-0">
+                  <div className="card-body">
+                    <strong>{countProducts}</strong> PRODUCTS
+                  </div>
+                  <div className="card-footer d-flex align-items-center justify-content-between">
+                    <Link
+                      className="small text-white stretched-link"
+                      to="/admin/products"
+                    >
+                      View Details
+                    </Link>
+                    <div className="small text-white">
+                      <i className="fas fa-angle-right"></i>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            )}
 
-            <div className="col-xl-3 col-md-6">
-              <div className="card bg-danger text-white mb-4 border-0 shadow-0">
-                <div className="card-body">
-                  <strong>{countAparaturs}</strong> APARATURS
-                </div>
-                <div className="card-footer d-flex align-items-center justify-content-between">
-                  <Link
-                    className="small text-white stretched-link"
-                    to="/admin/aparaturs"
-                  >
-                    View Details
-                  </Link>
-                  <div className="small text-white">
-                    <i className="fas fa-angle-right"></i>
+            {hasAnyPermission(["aparaturs.index"]) && (
+              <div className="col-xl-3 col-md-6">
+                <div className="card bg-danger text-white mb-4 border-0 shadow-0">
+                  <div className="card-body">
+                    <strong>{countAparaturs}</strong> APARATURS
+                  </div>
+                  <div className="card-footer d-flex align-items-center justify-content-between">
+                    <Link
+                      className="small text-white stretched-link"
+                      to="/admin/aparaturs"
+                    >
+                      View Details
+                    </Link>
+                    <div className="small text-white">
+                      <i className="fas fa-angle-right"></i>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </main>
