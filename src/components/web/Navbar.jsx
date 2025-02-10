e8f04b85cb41b9be0f1db6dd190d8564984782ca
kodri01 <kodrimukhtar5@gmail.com>
2025-02-10 20:11:54 +0700

    fix: modify navbar style

--- src/components/web/Navbar.jsx
+++ src/components/web/Navbar.jsx
@@ -85,89 +85,94 @@ export default function Navbar() {
           </div>
         </div>
       </div>
-      <nav className="navbar navbar-expand-md navbar-light navbar-blue navbar-web">
+      <nav className="navbar navbar-expand-md navbar-light navbar-blue nav-web">
         <div className="container">
           <button
-            type="button"
             className="navbar-toggler"
+            type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarCollapse"
             aria-controls="navbarCollapse"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarCollapse">
-            <ul className="navbar-nav me-auto mb-2 mb-md 0">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item ms-2">
                 <Link
-                  to="/"
                   className={
                     activeRoute[1] === ""
                       ? "nav-link active text-uppercase"
                       : "nav-link text-uppercase"
                   }
+                  to="/"
                 >
                   <i className="fa fa-home"></i> BERANDA
                 </Link>
               </li>
+
               <li className="nav-item ms-2">
                 <Link
-                  to="/pages"
                   className={
                     activeRoute[1] === "pages"
                       ? "nav-link active text-uppercase"
                       : "nav-link text-uppercase"
                   }
+                  to="/pages"
                 >
                   <i className="fa fa-info-circle"></i> TENTANG DESA
                 </Link>
               </li>
+
               <li className="nav-item ms-2">
                 <Link
-                  to="/aparaturs"
                   className={
                     activeRoute[1] === "aparaturs"
                       ? "nav-link active text-uppercase"
                       : "nav-link text-uppercase"
                   }
+                  to="/aparaturs"
                 >
                   <i className="fa fa-user-circle"></i> APARATURS
                 </Link>
               </li>
+
               <li className="nav-item ms-2">
                 <Link
-                  to="/posts"
                   className={
                     activeRoute[1] === "posts"
                       ? "nav-link active text-uppercase"
                       : "nav-link text-uppercase"
                   }
+                  to="/posts"
                 >
                   <i className="fa fa-book"></i> BERITA
                 </Link>
               </li>
+
               <li className="nav-item ms-2">
                 <Link
-                  to="/products"
                   className={
                     activeRoute[1] === "products"
                       ? "nav-link active text-uppercase"
                       : "nav-link text-uppercase"
                   }
+                  to="/products"
                 >
                   <i className="fa fa-shopping-bag"></i> PRODUK DESA
                 </Link>
               </li>
+
               <li className="nav-item ms-2">
                 <Link
-                  to="/photos"
                   className={
                     activeRoute[1] === "photos"
                       ? "nav-link active text-uppercase"
                       : "nav-link text-uppercase"
                   }
+                  to="/photos"
                 >
                   <i className="fa fa-images"></i> GALERI
                 </Link>
